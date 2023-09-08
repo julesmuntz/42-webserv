4a08c4553bbebedefba4fca3823ba76f5888194b
Maha BELRHAZI <mbelrhaz@made-f0Dr5s5.clusters.42paris.fr>
2023-09-08 19:32:04 +0200

    timeout implemented

--- obj/RequestEndDeterminator.d
+++
@@ -1,3 +0,0 @@
-obj/RequestEndDeterminator.o: srcs/RequestEndDeterminator.cpp \
- include/Server.hpp include/RequestHandler.hpp \
- include/RequestEndDeterminator.hpp
--- obj/main.d
+++
@@ -1,2 +0,0 @@
-obj/main.o: srcs/main.cpp include/Server.hpp include/RequestHandler.hpp \
- include/RequestEndDeterminator.hpp
