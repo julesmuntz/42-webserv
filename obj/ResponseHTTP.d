fda1b8b8731520186776f090bb4e3d2270512cdb
Maha BELRHAZI <mbelrhaz@made-f0Ar10s1.clusters.42paris.fr>
2023-09-22 22:59:43 +0200

    get method functioning a bit

--- obj/ResponseHTTP.d
+++
@@ -1,4 +0,0 @@
-obj/ResponseHTTP.o: srcs/ResponseHTTP.cpp include/ResponseHTTP.hpp \
- include/ResponseError.hpp include/Server.hpp include/RequestHandler.hpp \
- include/RequestEndDeterminator.hpp include/RequestParser.hpp \
- include/utils.hpp include/ResponseSender.hpp include/ResponseDir.hpp
--- obj/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.o: srcs/main.cpp include/Server.hpp include/RequestHandler.hpp \
- include/RequestEndDeterminator.hpp include/RequestParser.hpp \
- include/utils.hpp include/ResponseSender.hpp include/Exodus.hpp
