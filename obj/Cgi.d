1ee151d1726f7aabecc2380203edcaf266310eb6
ThzEdouard <edouard.flaquet@gmail.com>
2023-10-23 17:55:12 +0200

    defaut

---
+++ obj/Cgi.d
@@ -0,0 +1,4 @@
+obj/Cgi.o: srcs/Cgi.cpp include/ResponseHTTP.hpp \
+ include/ResponseError.hpp include/Server.hpp include/RequestHandler.hpp \
+ include/RequestEndDeterminator.hpp include/RequestParser.hpp \
+ include/utils.hpp include/struct.hpp include/ResponseSender.hpp
---
+++ obj/ResponseGETPOST.d
@@ -0,0 +1,5 @@
+obj/ResponseGETPOST.o: srcs/ResponseGETPOST.cpp include/Server.hpp \
+ include/RequestHandler.hpp include/RequestEndDeterminator.hpp \
+ include/RequestParser.hpp include/utils.hpp include/struct.hpp \
+ include/ResponseSender.hpp include/ResponseHTTP.hpp \
+ include/ResponseError.hpp include/ResponseDir.hpp
