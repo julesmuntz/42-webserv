e7ec7cab3a757e6de2054914f975b45d7d93ffa1
Maha BELRHAZI <mbelrhaz@paul-f4Ar2s2.clusters.42paris.fr>
2023-10-23 19:16:23 +0200

    shutting down server

--- obj/Cgi.d
+++
@@ -1,4 +0,0 @@
-obj/Cgi.o: srcs/Cgi.cpp include/ResponseHTTP.hpp \
- include/ResponseError.hpp include/Server.hpp include/RequestHandler.hpp \
- include/RequestEndDeterminator.hpp include/RequestParser.hpp \
- include/utils.hpp include/struct.hpp include/ResponseSender.hpp
