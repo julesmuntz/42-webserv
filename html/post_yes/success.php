625f6be18c3acd4d2857b8e4d3b3748316db26b6
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2023-10-24 17:27:10 +0200

    php page updated

--- html/post_yes/success.php
+++ html/post_yes/success.php
@@ -1,12 +1,27 @@
-Bonjour, <?php echo $_POST['username']; ?>.
-Ton email est <?php echo $_POST['email']; ?>.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-image: url("static/stage.png");
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
 
 <?php 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    echo "eeee $username";
-    // Utilisez $username et $email comme vous le souhaitez
-}
 
+    echo "<p>Votre username est : ".$_POST['username']." </p>" ;
+    echo "<p>Votre password est : ".$_POST['password']." </p>" ;
+    echo "<p>Votre email est : ".$_POST['email']." </p>" ;
+    echo "<p>Votre gender est : ".$_POST['gender']." </p>" ;
+    echo "<p>Votre interests est : ".$_POST['interests']." </p>" ;
+    echo "<p>Votre comments est : ".$_POST['comments']." </p>" ;
+    echo $_POST['fileUpload'];
 ?>
