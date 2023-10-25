ff0206751bdc2a652116d3c6ccdac10782e5cca2
Maha BELRHAZI <mbelrhaz@made-f0Br6s2.clusters.42paris.fr>
2023-10-25 11:44:37 +0200

    changed timeout to 10

--- obj/Exodus.d
+++
@@ -1,2 +0,0 @@
-obj/Exodus.o: srcs/Exodus.cpp include/Exodus.hpp include/struct.hpp \
- include/utils.hpp
--- obj/Server.d
+++
@@ -1,5 +0,0 @@
-obj/Server.o: srcs/Server.cpp include/Server.hpp \
- include/RequestHandler.hpp include/RequestEndDeterminator.hpp \
- include/RequestParser.hpp include/utils.hpp include/struct.hpp \
- include/ResponseSender.hpp include/ResponseHTTP.hpp \
- include/ResponseError.hpp
